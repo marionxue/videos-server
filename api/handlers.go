087426341fb58015cab6d9ee11eec236cd9590dd
marionxue <you@example.com>
2019-12-19 23:50:21 +0800

    Remove comments

--- api/handlers.go
+++ api/handlers.go
@@ -10,7 +10,7 @@ func CreateUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	io.WriteString(w, "Create User Handler")
 }
 
-// 登录 Method: POST, SC: 200,400,500
+
 func Login(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 	uname := p.ByName("user_name")
 	io.WriteString(w, uname)
